c953e265ac27398c7a3b901ee51c8ba18f68d2b4
Nandana <33281538+nandanasugn@users.noreply.github.com>
2022-12-11 20:10:47 +0700

    Create Reduce.java

---
+++ Reduce.java
@@ -0,0 +1,19 @@
+public class NBCReduce extends Reducer<IntWritable, Text, IntWritable, Text>{
+    public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+        Double out_value=Double.NEGATIVE_INFINITY;
+        String out_key=null;
+        HashMap<String,Integer> final_output=new HashMap<String,Integer>();
+        for(Text value:values){
+            if(final_output.containsKey(value.toString()))
+                final_output.put(value.toString(), final_output.get(value.toString())+1);
+            else
+                final_output.put(value.toString(), 1);
+        }
+        for(Entry<String,Integer> output:final_output.entrySet()){
+            if(output.getValue()>out_value){
+                out_value=(double) output.getValue();
+                out_key=output.getKey();
+            }
+        }context.write(null, new Text(out_key));
+    }
+}
